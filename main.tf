e94f8b80a1c4c353f4015b660be38ff66a29b6bd
Charles Kim <cysk92@gmail.com>
2022-09-19 10:12:34 -0400

    Update tf

--- main.tf
+++ main.tf
@@ -14,3 +14,11 @@ resource "aws_s3_bucket_public_access_block" "cloudrail" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket_public_access_block" "cloudrail-2" {
+  bucket                  = aws_s3_bucket.cloudrail-2.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
